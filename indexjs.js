fe09cb054da6e80f0020fde07c480e62c317a8c0
sufian1 <msbustamam@gmail.com>
2021-10-19 17:38:34 +0800

    add basic html skeleton

---
+++ indexjs.js
@@ -0,0 +1,13 @@
+console.log("hello!")
+console.log("check it...!")
+
+const person = {name: 'timothy', age: 58, talk:greet}
+
+let msg = 'I hate mayo!';
+
+function greet(){
+    let msg='hiiii'
+    console.log(msg);
+}
+
+console.log(msg); 
